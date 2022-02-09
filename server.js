cef19cdb7dd2bf50a47a653b3d5a5ad2816d7989
Shubham Verma <77584464+sshubham681@users.noreply.github.com>
2022-02-10 00:41:47 +0530

    routing done

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 require("dotenv/config");
+
 const connectDB = require("./config/db");
 
 connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
